c5df815173c9faa7debe812cf471dc4a69137ecb
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-06-23 19:10:44 +0200

    refactored app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-import FRCLogo from "/logo2.webp"
-// import './App.css'
-import "./DiceRollAnimation";
-import DiceRollAnimation from './components/DiceRollAnimation'
-import WalletSelectHeader from "./components/WalletSelectHeader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotFoundPage } from "./pages/404Page";
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { styled } from "@mui/material";
+
 import Snackbar from '@mui/material/Snackbar';
 import * as React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
@@ -17,16 +10,6 @@ import GamePage from "./pages/GamePage";
 import MintPage from "./pages/MintPage";
 import LandingPage from "./pages/LandingPage";
 
-
-
-
-//TODO: needs a connect wallet popup  
-enum Pages {
-  LANDINGPAGE = 0,
-  MINTNFT = 1,
-  DICEGAME = 2, // Needs to have the ID for the game, dynamic link
-  GAMESEARCH = 3 // A page for the indexer that allows users to search games
-}
 
 //TODO: Each page should have the same walletSelectAppbar
 //The pages should be separate components that each fetch data without connected wallet
