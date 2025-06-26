4addbc8c61dd255cda33854042088ac189e166e1
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-06-26 19:12:11 +0200

    refactored game page

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,13 +1,19 @@
-// UserProfile.jsx
 import { useParams } from 'react-router-dom';
-import WalletSelectHeader from "../components/WalletSelectHeader";
-
+import BettingPool from '../components/BettingPool';
+import DixyDiceAssistant from '../components/Dixy';
+import { Stack, Box } from "@mui/material"
+import DiceRollHistory from '../components/DiceRollHistory';
 
 
 export default function GamePage() {
     const { id } = useParams();
 
     return <div>
-        <WalletSelectHeader></WalletSelectHeader>
-        <div>Game ID: {id}</div></div>;
+        <div>Game ID: {id}</div>
+        <Stack height={"70vh"} direction={"column"} justifyContent="space-between">
+            <Box><BettingPool gameId={id} /></Box>
+            <Box><DixyDiceAssistant /></Box>
+            <Box><DiceRollHistory /></Box>
+        </Stack>
+    </div>;
 }
