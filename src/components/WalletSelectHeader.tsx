0e8e1ec2c2bb9b2226bf2a6d9a336f73d9fb7cb7
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-06-26 17:06:36 +0200

    wallet select implemented

--- src/components/WalletSelectHeader.tsx
+++ src/components/WalletSelectHeader.tsx
@@ -5,8 +5,26 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import FRCLogo from "/logo2.webp"
+import '../web3/fcl'; //The config runs here
+import { useState, useEffect } from 'react'
+import * as fcl from "@onflow/fcl"
 
 export default function WalletSelectHeader() {
+
+    const [user, setUser] = useState({ loggedIn: null, addr: "" })
+
+    useEffect(() => {
+        fcl.currentUser.subscribe(setUser)
+    }, [])
+
+    const signIn = () => {
+        fcl.logIn()
+    }
+
+    const signOut = () => {
+        fcl.unauthenticate()
+    }
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" color='transparent'>
@@ -23,7 +41,16 @@ export default function WalletSelectHeader() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Flow Roll Club
                     </Typography>
-                    <Button color="inherit">Connect wallet</Button>
+                    <div>
+                        {user.loggedIn ? (
+                            <div>
+                                {/* <p>Address: {user.addr}</p> */}
+                                <Button color="inherit" onClick={signOut}>Sign Out</Button>
+                            </div>
+                        ) : (
+                            <Button color="inherit" onClick={signIn}>Connect Wallet</Button>
+                        )}
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>
