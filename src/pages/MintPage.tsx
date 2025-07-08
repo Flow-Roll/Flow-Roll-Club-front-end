5ac71c5c5931b6dfb7b655c1b1496f600deb1ddd
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-07-08 22:39:45 +0200

    added snackbar to mintpage

--- src/pages/MintPage.tsx
+++ src/pages/MintPage.tsx
@@ -1,5 +1,5 @@
 import AnimatedBettingForm from "../components/GameConfiguration"
 
-export default function MintPage() {
-    return <div><AnimatedBettingForm></AnimatedBettingForm></div>
+export default function MintPage(props: { openSnackbar: CallableFunction }) {
+    return <div><AnimatedBettingForm openSnackbar={props.openSnackbar} ></AnimatedBettingForm></div>
 }
